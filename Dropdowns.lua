455e368c5c914b69a58f1076263835f4b1f43e39
Rhinonuts11 <161979906+Rhinonuts11@users.noreply.github.com>
2024-03-02 21:58:44 +0000

    Add files via upload

---
+++ Dropdowns.lua
@@ -0,0 +1,25 @@
+Dropdowns.BehaviourDropdownPages = {
+	["BehaviouralIncidents"] = {
+		System:WaitForChild("BehaviourDashboard"),
+		System:WaitForChild("BehaviourDashboard").IncidentDashboardPage
+	},
+	["Merits"] = {
+		System:WaitForChild("BehaviourDashboard"),
+		System:WaitForChild("BehaviourDashboard").MeritsDashboard
+	},
+	["Detentions"] = {
+		System:WaitForChild("DetentionsPage")
+	},
+	["BehaviourPoints"] = {
+		System:WaitForChild("BehaviourDashboard"),
+		System:WaitForChild("BehaviourDashboard").BehaviourPointsDashboard
+	},
+	["Isolations"] = {
+		System:WaitForChild("BehaviourDashboard"),
+		System:WaitForChild("BehaviourDashboard").IsolationsDashboard
+	},
+	["Timeouts"] = {
+		System:WaitForChild("BehaviourDashboard"),
+		System:WaitForChild("BehaviourDashboard").TimeoutDashboard
+	}
+}
